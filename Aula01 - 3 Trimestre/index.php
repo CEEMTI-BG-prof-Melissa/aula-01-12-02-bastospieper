abb9bcee419904aba0c7dc4ccb8a6c8d788a6fa9
MiguellPieper <miguell.pmeireles@aluno.edu.es.gov.br>
2025-09-03 11:57:37 -0300

    Update index.php

--- Aula01 - 3 Trimestre/index.php
+++ Aula01 - 3 Trimestre/index.php
@@ -1,1 +1,11 @@
-
+<html>
+    <head>
+    </head>
+        <title> Aula 01 PHP </title>
+    <body>
+        <?php
+            echo "Olá, Mundo!";
+            echo "Sofia Bastos e Miguell Pieper";
+        ?>
+    </body>
+</html>
